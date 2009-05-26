e05a446ce198b94227b03623051475448237d9b9
Jacob Kaplan-Moss <jacob@jacobian.org>
2009-05-27 06:57:38 +0800

    Added a geocoder for Yahoo's (non-placemark) geocoder.
    Signed-off-by: Simon Willison <simon@simonwillison.net>

---
+++ yahoo.py
@@ -0,0 +1,38 @@
+from utils import make_nsfind, ET, partial2
+import urllib
+
+def geocode(q, api_key):
+    find = make_nsfind({'ns': 'urn:yahoo:maps'})
+    args = {'location': q, 'appid': api_key}
+    url = 'http://local.yahooapis.com/MapsService/V1/geocode?%s' % urllib.urlencode(args)
+    et = ET.parse(urllib.urlopen(url))
+    
+    result = find(et, '//ns:Result')
+    if not result:
+        return (None, (None, None))
+    else:
+        namebits = {}
+        for field in ('Address', 'City', 'State', 'Zip', 'Country'):
+            bit = find(result, 'ns:%s' % field)
+            if bit is not None and bit.text:
+                namebits[field] = bit.text.decode('utf8')
+
+        if 'Address' in namebits:
+            name = '%(Address)s, %(City)s, %(State)s %(Zip)s, %(Country)s' % namebits
+        elif 'Zip' in namebits:
+            name = '%(City)s, %(State)s %(Zip)s, %(Country)s' % namebits
+        elif 'City' in namebits:
+            name = '%(City)s, %(State)s, %(Country)s' % namebits
+        elif 'State' in namebits:
+            name = '%(State)s, %(Country)s' % namebits
+        elif 'Country' in namebits:
+            name = namebits['Country']
+        else:
+            return (None, (None, None))
+
+        lat = float(find(result, 'ns:Latitude').text)
+        lon = float(find(result, 'ns:Longitude').text)
+        
+        return (name, (lat, lon))
+    
+geocoder = partial2(geocode)
