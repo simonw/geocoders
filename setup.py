b3d7c4fcceffb90b1a085d1d07af9f120e104b45
Mark Ng <mark@markng.co.uk>
2010-01-27 13:38:27 +0800

    setup script, ignore build directory

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name = 'Geocoders',
+    version = '0.1',
+    author = 'Simon Willison',
+    url = 'http://github.com/simonw/geocoders',
+    packages = ['geocoders'],
+)
